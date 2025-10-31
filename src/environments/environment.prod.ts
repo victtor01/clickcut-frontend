692f059a43cc2c5d6b954b511d8f5793f04ff8ab
victtor01 <josevictot.ar@gmail.com>
2025-10-30 23:22:49 -0300

    modified apiUrl

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://xm0mcfva6939.share.zrok.io/api',
+  apiUrl: 'https://borough-novelty-norman-loc.trycloudflare.com/api',
   // apiUrl: 'https://q5frp03ij64b.share.zrok.io/api',
   // apiUrl: '/api'
   // apiUrl: 'http://localhost:5055/api',
