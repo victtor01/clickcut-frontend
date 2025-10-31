4abda51a12361a37243fd466ad5d001f438c1397
victtor01 <josevictot.ar@gmail.com>
2025-10-31 15:31:04 -0300

    modified url api

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://manuals-affordable-paintball-decision.trycloudflare.com/api',
+  apiUrl: 'https://instruction-extensions-done-index.trycloudflare.com/api',
   // apiUrl: 'https://q5frp03ij64b.share.zrok.io/api',
   // apiUrl: 'http://localhost:5500/api',
 };
