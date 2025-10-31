d1bab6844859f5759eaae6a964a67c72b7742fa2
victtor01 <josevictot.ar@gmail.com>
2025-10-31 15:20:12 -0300

    new apiUrl

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://manuals-affordable-paintball-decision.trycloudflare.com/api',
+  apiUrl: 'https://instruction-extensions-done-index.trycloudflare.com/api',
   // apiUrl: 'https://q5frp03ij64b.share.zrok.io/api',
   // apiUrl: 'https://10.220.0.8:7001/api',
 };
