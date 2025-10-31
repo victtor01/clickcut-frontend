17e86be2be92a8ba4590f3b1ec9d2fb7485b850f
victtor01 <josevictot.ar@gmail.com>
2025-10-31 14:48:36 -0300

    api-url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://borough-novelty-norman-loc.trycloudflare.com/api',
+  apiUrl: 'https://manuals-affordable-paintball-decision.trycloudflare.com/api',
   // apiUrl: 'https://q5frp03ij64b.share.zrok.io/api',
-  // apiUrl: '/api'
-  // apiUrl: 'http://localhost:5055/api',
+  // apiUrl: 'https://10.220.0.8:7001/api',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://borough-novelty-norman-loc.trycloudflare.com/api',
+  apiUrl: 'https://manuals-affordable-paintball-decision.trycloudflare.com/api',
   // apiUrl: 'https://q5frp03ij64b.share.zrok.io/api',
-  // apiUrl: 'http://localhost:5055/api',
+  // apiUrl: 'http://localhost:5500/api',
 };
